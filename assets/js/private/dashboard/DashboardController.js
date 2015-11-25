aabdf57fe461a8c4965b1c0855adc5910e3a147a
Pierre FABIER <pierre.fabier@gmail.com>
2015-11-25 14:48:45 +0100

    Implémentation basique du dashboard avec des images génériques

--- assets/js/private/dashboard/DashboardController.js
+++ assets/js/private/dashboard/DashboardController.js
@@ -1,1 +1,29 @@
-angular.module('DashboardModule').controller('DashboardController', [ function(){}]);
+
+angular.module('DashboardModule').controller('DashboardController', ['$scope', '$http', 'toastr', function ($scope, $http, toastr) {
+
+        var categories = ['abstract',
+            'animals',
+            'business',
+            'cats',
+            'city',
+            'food',
+            'nightlife',
+            'fashion',
+            'people',
+            'nature',
+            'sports',
+            'technics',
+            'transport'];
+
+
+        $scope.needHelp = [];
+        for (var i = 0; i < 12; i++) {
+            $scope.needHelp.push({value: i % 4, category: categories[Math.floor(Math.random() * categories.length)]});
+        }
+
+        $scope.hasFoundMatchingClothes = [];
+        for (var i = 0; i < 24; i++) {
+            $scope.hasFoundMatchingClothes.push({value: i % 4, category: categories[Math.floor(Math.random() * categories.length)]});
+        }
+
+    }]);
