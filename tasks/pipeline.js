ec05eae3d618b1d33fc0ad5d108e2b21d447d552
Pierre FABIER <pierre.fabier@gmail.com>
2015-11-25 13:12:35 +0100

    Ajout de angular et des controlleurs dans le pipeline

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -16,26 +16,26 @@ var tmpPath = '.tmp/public/';
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+    'styles/**/*.css'
 ];
 
 
 // Client-side javascript files to inject in order
 // (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
-
-  // Load sails.io before everything else
-  'js/dependencies/sails.io.js',
-
-  // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
-
-  // All of the rest of your client-side js files
-  // will be injected here in no particular order.
-  'js/**/*.js',
-
-  // Use the "exclude" operator to ignore files
-  // '!js/ignore/these/files/*.js'
+    // Load sails.io before everything else
+    'js/dependencies/sails.io.js',
+    // Dependencies like jQuery, or Angular are brought in here
+    'js/dependencies/angular.1.3.js',
+    'js/dependencies/**/*.js',
+    // All of the rest of your client-side js files
+    // will be injected here in no particular order.
+    'js/public/signup/SignupModule.js',
+    'js/private/dashboard/DashboardModule.js',
+    'js/public/homepage/HomepageModule.js',
+    'js/**/*.js',
+            // Use the "exclude" operator to ignore files
+            // '!js/ignore/these/files/*.js'
 ];
 
 
@@ -49,7 +49,7 @@ var jsFilesToInject = [
 // templates get spit out to the same file.  Be sure and check out `tasks/README.md`
 // for information on customizing and installing new tasks.
 var templateFilesToInject = [
-  'templates/**/*.html'
+    'templates/**/*.html'
 ];
 
 
@@ -64,5 +64,5 @@ module.exports.templateFilesToInject = templateFilesToInject.map(transformPath);
 // Transform paths relative to the "assets" folder to be relative to the public
 // folder, preserving "exclude" operators.
 function transformPath(path) {
-  return (path.substring(0,1) == '!') ? ('!' + tmpPath + path.substring(1)) : (tmpPath + path);
+    return (path.substring(0, 1) === '!') ? ('!' + tmpPath + path.substring(1)) : (tmpPath + path);
 }
