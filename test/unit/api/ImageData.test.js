23993d7ef12ddf57d69cd24024a5a8153de21e69
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-03 11:07:08 +0100

    Amélioration des test unitaires pour les données image

--- test/unit/api/ImageData.test.js
+++ test/unit/api/ImageData.test.js
@@ -4,6 +4,7 @@ var request = require('supertest');
 var faker = require('faker');
 var randomId = require('random-id');
 var winston = require('winston');
+var md5 = require('md5');
 var fs = require('fs');
 var pixnista = require('../../fixtures/pixnista.js');
 
@@ -11,7 +12,11 @@ var pixnista = require('../../fixtures/pixnista.js');
 var imageDataBinary = null;
 
 var imageData = {
+    id: null,
+    path: './test/images/small.jpg',
+    pathUpdate: './test/images/large.jpg',
     data: null,
+    md5: null,
     creator: null
 };
 
@@ -22,11 +27,13 @@ describe('ImageData API', function () {
 
     // A executer avant de commencer les tests
     before(function (done) {
-        fs.readFile('./test/images/small.jpg', function (err, data) {
+        request = request(pixnista.baseURL());
+        fs.readFile(imageData.path, function (err, data) {
             if (err) {
                 throw err;
             }
             imageData.data = data;
+            imageData.md5 = md5(data);
             pixnista.findUser(null, function (err, u) {
                 user = u;
                 imageData.creator = user.id;
@@ -36,33 +43,39 @@ describe('ImageData API', function () {
     });
 
     it('should be able to post a new ImageData', function (done) {
-        request(baseURL()).post('/imageData')
-                .field('extra_info', '{"in":"case you want to send json along with your file"}')
-                .attach('image', './test/images/small.jpg')
+        request.post('/imageData')
+//                .field('extra_info', '{"in":"case you want to send json along with your file"}')
+                .attach('image', imageData.path)
                 .end(function (err, res) {
+                    imageData.id = res.body[0].id;
                     pixnista.handleResponseCheckStatusCode(err, res, 201, done);
                 });
     });
     it('should be able to get the newly created ImageData', function (done) {
-//        request(baseURL()).get('/imageData/' + imageData.id).end(function (err, res) {
-//            pixnista.handleResponseCheckStatusCode(err, res, 200, done);
-//        });
-        done();
+        request.get('/imageData/' + imageData.id).end(function (err, res) {
+            pixnista.handleResponseCheckStatusCode(err, res, 200);
+            var md5downloaded = md5(res.body);
+            md5downloaded.should.equal(imageData.md5);
+            done();
+        });
     });
-    it('should be able to delete a Image', function (done) {
-//        request(baseURL()).delete('/imageData/' + imageData.id).end(function (err, res) {
-//            pixnista.handleResponseCheckStatusCode(err, res, 200, done);
-//        });
-        done();
+    it('should not be able to update a ImageData', function (done) {
+        request.put('/imageData/' + imageData.id)
+//                .field('extra_info', '{"in":"case you want to send json along with your file"}')
+                .attach('image', imageData.pathUpdate)
+                .end(function (err, res) {
+                    pixnista.handleResponseCheckStatusCode(err, res, 405, done);
+                });
     });
-    it('should not be able to get a deleted Image', function (done) {
-//        request(baseURL()).get('/imageData/' + imageData.id).end(function (err, res) {
-//            pixnista.handleResponseCheckStatusCode(err, res, 404, done);
-//        });
-        done();
+    it('should not be able to delete a ImageData', function (done) {
+        request.delete('/imageData/' + imageData.id).end(function (err, res) {
+            pixnista.handleResponseCheckStatusCode(err, res, 405, done);
+        });
+    });
+    it('should still be able to get the ImageData we just tried to delete', function (done) {
+        request.get('/imageData/' + imageData.id).end(function (err, res) {
+            pixnista.handleResponseCheckStatusCode(err, res, 200);
+            done();
+        });
     });
 });
-
-function baseURL() {
-    return pixnista.baseURL();
-}
