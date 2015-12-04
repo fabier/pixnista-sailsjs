0e62d1152aea94cf0a06c8d69a126d6e8addbcaf
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-04 09:48:44 +0100

    Modification de la méthode de create pour un PostVote
    On interdit à un utilisateur de voter deux fois sur un même post
    On ne peut pas supprimer un vote
    On ne peut pas mettre à jour un vote

--- api/controllers/PostVoteController.js
+++ api/controllers/PostVoteController.js
@@ -6,6 +6,39 @@
  */
 
 module.exports = {
-	
+    create: function (req, res) {
+        var postVote = {
+            vote: req.param('vote'),
+            comment: req.param('comment'),
+            post: req.param('post'),
+            creator: req.param('creator'),
+            voteReason: req.param('voteReason')
+        };
+        // On cherche si un vote existe déjà pour ce post
+        PostVote.findOne({post: postVote.post, creator: postVote.creator}, function (err, postVoteExists) {
+            if (err) {
+                return res.negotiate(err);
+            } else if (postVoteExists) {
+                // Si l'utilisateur a déjà voté, il ne peut pas voter une nouvelle fois
+                res.status(403).send("It's forbidden to vote twice on the same post");
+            } else {
+                // Si l'utilisateur n'a pas encore voté, alors on prend en compte son vote
+                PostVote.create(postVote, function (err, postVote) {
+                    if (err) {
+                        return res.negotiate(err);
+                    } else {
+                        // Send back the created PostVote
+                        return res.status(201).json(postVote);
+                    }
+                });
+            }
+        });
+    },
+    update: function (req, res) {
+        res.forbidden();
+    },
+    destroy: function (req, res) {
+        res.forbidden();
+    }
 };
 
