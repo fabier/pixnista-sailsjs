e80a65ecbeef027e2031c055705c0b01abd89a08
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-06 17:41:53 +0100

    Modification du bootstrap pour ne s'executer que si les données ne sont pas encore initialisées

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -14,37 +14,51 @@ var async = require('async');
 
 module.exports.bootstrap = function (cb) {
 
-    // Initialisation des listes "statiques"
-    sails.log.info("#######################");
-    sails.log.info("## DEBUT : Bootstrap ##");
-    sails.log.info("#######################");
-
-    async.parallel([
-        LanguageService.init,
-        CountryService.init,
-        VisibilityService.init,
-        BodyTypeService.init,
-        FashionStyleService.init,
-        StateService.init,
-        VoteReasonService.init,
-        ImageTypeService.init,
-        PostTypeService.init,
-        UserService.init,
-    ], function (err) {
+    // On regarde s'il y a déjà des données dans la base de données
+    User.count(function (err, count) {
         if (err) {
             throw err;
-        }
-        sails.log.info("#####################");
-        sails.log.info("## FIN : Bootstrap ##");
-        sails.log.info("#####################");
-        // It's very important to trigger this callback method when you are finished
-        // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-        cb();
+        } else {
+            if (count === 0) {
+                // Initialisation des listes "statiques"
+                sails.log.info("#######################");
+                sails.log.info("## DEBUT : Bootstrap ##");
+                sails.log.info("#######################");
 
-        if (sails.config.environment === 'development') {
-            setTimeout(function () {
-                FakerService.init(function () {});
-            }, 1000);
+                // Pas encore de données en base
+                // On initialise
+                async.parallel([
+                    LanguageService.init,
+                    CountryService.init,
+                    VisibilityService.init,
+                    BodyTypeService.init,
+                    FashionStyleService.init,
+                    StateService.init,
+                    VoteReasonService.init,
+                    ImageTypeService.init,
+                    PostTypeService.init,
+                    UserService.init,
+                ], function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    sails.log.info("#####################");
+                    sails.log.info("## FIN : Bootstrap ##");
+                    sails.log.info("#####################");
+                    // It's very important to trigger this callback method when you are finished
+                    // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+                    cb();
+
+                    if (sails.config.environment === 'development') {
+                        setTimeout(function () {
+                            FakerService.init(function () {});
+                        }, 1000);
+                    }
+                });
+            } else {
+                sails.log.info("Bootstrap.js : Données déjà initialisées");
+                cb();
+            }
         }
     });
 };
