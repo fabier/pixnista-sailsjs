e44bdf2b9a3f84df78d5f588f7455645d3080db6
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-06 17:40:58 +0100

    Ajout de la méthode d'affichage des données d'une image par son identifiant

--- api/controllers/ImageController.js
+++ api/controllers/ImageController.js
@@ -11,6 +11,27 @@ module.exports = {
      */
     find: function (req, res) {
         res.forbidden();
+    },
+    show: function (req, res) {
+        Image.findOne(req.param('id')).populate('imageData').exec(function (err, image) {
+            if (err) {
+                res.negotiate(err);
+            } else {
+                if (image) {
+                    var imageData = image.imageData;
+                    if (imageData) {
+                        res.set('Content-Type', imageData.type);
+                        res.send(imageData.data);
+                    } else {
+                        // Pas de contenu trouvé
+                        res.notFound();
+                    }
+                } else {
+                    // Pas de contenu trouvé
+                    res.notFound();
+                }
+            }
+        });
     }
 };
 
