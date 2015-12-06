bfb4661b4c7e87bc2f30fbe8372b6ed9bc8e612f
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-06 17:41:18 +0100

    Ajout des méthodes de recherche de Post par type de post

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -11,6 +11,29 @@ module.exports = {
      */
     find: function (req, res) {
         res.forbidden();
+    },
+    help: function (req, res) {
+        findPostsByPostType('help', req, res);
+    },
+    dressing: function (req, res) {
+        findPostsByPostType('dressing', req, res);
     }
 };
+
+function findPostsByPostType(postTypeName, req, res) {
+    PostType.findOne({name: postTypeName}).exec(function (err, postType) {
+        if (err) {
+            res.negotiate(err);
+        } else {
+            var limit = Math.min(req.param('limit') || 30, 30);
+            Post.find({postType: postType.id}).limit(limit).populate('images').exec(function (err, posts) {
+                if (err) {
+                    res.negotiate(err);
+                } else {
+                    res.json(posts);
+                }
+            });
+        }
+    });
+}
 
