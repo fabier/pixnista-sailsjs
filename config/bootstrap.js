efedf4a675f5262c858e9fe3aa14e0622559a476
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-06 02:00:12 +0100

    Simplification du code de bootstrap pour prendre en compte les nouvelles formes des méthodes d'init des services

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,48 +9,42 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-module.exports.bootstrap = function (cb) {
-    sails.sequence = require('sequence');
+// Pour lancer les initialisations dans un ordre prédéfini
+var async = require('async');
 
-    // Pour lancer les initialisations dans un ordre prédéfini
-    var sequence = sails.sequence.Sequence.create();
+module.exports.bootstrap = function (cb) {
 
     // Initialisation des listes "statiques"
-    sequence.then(function (next) {
-        sails.log.info("#######################");
-        sails.log.info("## DEBUT : Bootstrap ##");
-        sails.log.info("#######################");
-        next();
-    }).then(function (next) {
-        LanguageService.init(null, next);
-    }).then(function (next) {
-        CountryService.init(null, next);
-    }).then(function (next) {
-        VisibilityService.init(null, next);
-    }).then(function (next, err) {
-        BodyTypeService.init(null, next);
-    }).then(function (next, err) {
-        FashionStyleService.init(null, next);
-    }).then(function (next, err) {
-        StateService.init(null, next);
-    }).then(function (next, err) {
-        VoteReasonService.init(null, next);
-    }).then(function (next, err) {
-        ImageTypeService.init(null, next);
-    }).then(function (next, err) {
-        UserService.init(null, next);
-    }).then(function (next, err) {
-        PostService.init(null, next);
-    }).then(function (next, err) {
+    sails.log.info("#######################");
+    sails.log.info("## DEBUT : Bootstrap ##");
+    sails.log.info("#######################");
+
+    async.parallel([
+        LanguageService.init,
+        CountryService.init,
+        VisibilityService.init,
+        BodyTypeService.init,
+        FashionStyleService.init,
+        StateService.init,
+        VoteReasonService.init,
+        ImageTypeService.init,
+        PostTypeService.init,
+        UserService.init,
+    ], function (err) {
+        if (err) {
+            throw err;
+        }
         sails.log.info("#####################");
         sails.log.info("## FIN : Bootstrap ##");
         sails.log.info("#####################");
-        next();
-    }).then(function (next, err) {
         // It's very important to trigger this callback method when you are finished
         // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
         cb();
+
+        if (sails.config.environment === 'development') {
+            setTimeout(function () {
+                FakerService.init(function () {});
+            }, 1000);
+        }
     });
-
-
 };
