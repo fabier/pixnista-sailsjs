54dc7201fe47cb8a2dd2903023d5ce1809161e8a
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-06 02:00:40 +0100

    UserService peut maintenant créer un ou N users en une seule fois

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -3,8 +3,11 @@
  *
  * @description :: Service for managing users
  */
+var async = require('async');
+var common = require('../common');
+
 module.exports = {
-    init: function (options, callback) {
+    init: function (callback) {
         User.find(null, function (err, values) {
             if (values.length === 0) {
                 sails.log.info("UserService : no users found, initializing...");
@@ -33,53 +36,69 @@ module.exports = {
      * @returns {undefined}
      */
     create: function (options, callback) {
-        var Passwords = require('machinepack-passwords');
-        // Encrypt a string using the BCrypt algorithm.
-        Passwords.encryptPassword({
-            password: options.password,
-            difficulty: 10
-        }).exec({
-            // An unexpected error occurred.
-            error: function (err) {
-                sails.log.warn("UserService : Impossible to create a User (step encrypt password)", options)
-                callback(err);
-            },
-            // OK.
-            success: function (encryptedPassword) {
-                require('machinepack-gravatar').getImageUrl({
-                    emailAddress: options.email
-                }).exec({
-                    error: function (err) {
-                        sails.log.warn("UserService : Impossible to create a User (step generate gravatar url)", options, err)
-                        callback(err);
-                    },
-                    success: function (gravatarUrl) {
-                        // Create a User with the params sent from
-                        // the sign-up form --> signup.ejs
-                        User.create({
-                            name: options.name,
-                            email: options.email,
-                            encryptedPassword: encryptedPassword,
-                            gravatarUrl: gravatarUrl
-                        }, function (err, user) {
-                            if (err) {
-                                sails.log.warn("UserService : Impossible to create a User (step User.create)", options, err)
-
-                                // If this is a uniqueness error about the email attribute,
-                                // send back an easily parseable status code.
-                                if (err.invalidAttributes && err.invalidAttributes.email && err.invalidAttributes.email[0]
-                                        && err.invalidAttributes.email[0].rule === 'unique') {
-                                    sails.log.warn("UserService : Impossible to create a User (step User.create : email already in use)", options)
-                                } else {
-                                    sails.log.warn("UserService : Impossible to create a User (step User.create)", options)
-                                }
-                            }
-
-                            callback(err, user);
-                        });
-                    }
+        if (options.constructor === Array) {
+            var users = [];
+            async.each(options, function (option, callback) {
+                createOneUser(option, function (err, user) {
+                    users.push(user);
+                    callback();
                 });
-            }
-        });
+            }, function (err) {
+                callback(err, users);
+            });
+        } else {
+            createOneUser(options, callback);
+        }
     }
 };
+
+function createOneUser(options, callback) {
+    var Passwords = require('machinepack-passwords');
+    // Encrypt a string using the BCrypt algorithm.
+    Passwords.encryptPassword({
+        password: options.password,
+        difficulty: 10
+    }).exec({
+// An unexpected error occurred.
+        error: function (err) {
+            sails.log.warn("UserService : Impossible to create a User (step encrypt password)", options)
+            callback(err);
+        },
+        // OK.
+        success: function (encryptedPassword) {
+            require('machinepack-gravatar').getImageUrl({
+                emailAddress: options.email
+            }).exec({
+                error: function (err) {
+                    sails.log.warn("UserService : Impossible to create a User (step generate gravatar url)", options, err)
+                    callback(err);
+                },
+                success: function (gravatarUrl) {
+                    // Create a User with the params sent from
+                    // the sign-up form --> signup.ejs
+                    User.create({
+                        name: options.name,
+                        email: options.email,
+                        encryptedPassword: encryptedPassword,
+                        gravatarUrl: gravatarUrl
+                    }, function (err, user) {
+                        if (err) {
+                            sails.log.warn("UserService : Impossible to create a User (step User.create)", options, err)
+
+                            // If this is a uniqueness error about the email attribute,
+                            // send back an easily parseable status code.
+                            if (err.invalidAttributes && err.invalidAttributes.email && err.invalidAttributes.email[0]
+                                    && err.invalidAttributes.email[0].rule === 'unique') {
+                                sails.log.warn("UserService : Impossible to create a User (step User.create : email already in use)", options)
+                            } else {
+                                sails.log.warn("UserService : Impossible to create a User (step User.create)", options)
+                            }
+                        }
+
+                        callback(err, user);
+                    });
+                }
+            });
+        }
+    });
+}
