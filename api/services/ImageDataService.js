0bff925106a7e51d9778e3c4ccc385b79fd13b20
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-09 22:31:13 +0100

    Ajout de la fonction d'enregistrement d'une image directement dans un service
    Notamment pour pouvoir être utilisé par les controlleurs Image et
    ImageData

--- api/services/ImageDataService.js
+++ api/services/ImageDataService.js
@@ -4,6 +4,7 @@
  * @description :: Service for managing visibilities (public, friends, private)
  */
 var md5 = require('md5');
+var fs = require('fs');
 
 module.exports = {
     /**
@@ -19,6 +20,28 @@ module.exports = {
                 sails.log.warn("ImageDataService : Impossible to create a ImageData", options, err);
             }
             callback(err, post);
+        });
+    },
+    saveFile: function (file, callback) {
+        fs.readFile(file.fd, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            ImageData.create({
+                data: data,
+                md5: md5(data),
+                type: file.type, // 'image/jpeg'
+                filename: file.filename // 'DSC_O892.JPG'
+            }, function (err, imageData) {
+                if (err) {
+                    callback(err);
+                } else {
+                    // Ce n'est pas important si la suppression ne fonctionne pas
+                    // donc on ne met pas de callback à l'appel de la fonction
+                    fs.unlink(file.fd); // Suppression du fichier temporaire
+                    callback(null, imageData);
+                }
+            });
         });
     }
 };
