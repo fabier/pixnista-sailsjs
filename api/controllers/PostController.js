8c9f21960567f0ffdee07efa56e6692e48f1000b
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-09 22:28:16 +0100

    Ajout de fonctions de création de Post
    Ajout de fonctionnalité permettant d'associer une image nouvellement
    crée à un post depuis les webservices

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -6,9 +6,20 @@
  */
 
 var moment = require('moment');
+var faker = require('faker');
+var mergeDefaults = require('merge-defaults');
 var _ = require('underscore');
 
 module.exports = {
+    create: function (req, res) {
+        var params = mergeDefaults(req.params.all(), {creator: req.session.user.id});
+        Post.create(params, function (err, post) {
+            if (err)
+                return res.negotiate(err);
+            // Send JSONP-friendly response if it's supported
+            res.created(post);
+        });
+    },
     /**
      * Il est interdit de lister les posts depuis l'API Rest
      */
@@ -21,6 +32,32 @@ module.exports = {
     dressing: function (req, res) {
         findPostsByPostType('dressing', req, res);
     },
+    new : function (req, res) {
+        if (!req.session.user) {
+//            res.set('WWW-Authenticate', 'Basic realm="pixnista.com":').status(401).send("Not Authorized");
+            res.view("auth/login", {faker: faker});
+        } else {
+            res.view("post/create", {faker: faker});
+        }
+    },
+    addToImages: function (req, res) {
+        var postId = req.param('id');
+        var imageId = req.param('imageId');
+        Post.findOne(req.param('id'), function (err, post) {
+            if (err) {
+                res.negotiate(err);
+            } else {
+                post.images.add(imageId);
+                post.save(function (err, post) {
+                    if (err) {
+                        res.negotiate(err);
+                    } else {
+                        res.json(post);
+                    }
+                });
+            }
+        });
+    },
     show: function (req, res) {
         Post.findOne(req.param('id'))
                 .populate("creator")
@@ -30,7 +67,9 @@ module.exports = {
                 .exec(function (err, post) {
                     if (err) {
                         res.negotiate(err);
-                    } else {
+                    } else if (post) {
+                        // On va aller chercher les informations relatives aux utilisateurs
+                        // qui ont posté des commentaires sur ce post
                         var creators = _.pluck(post.postComments, 'creator');
                         creators = _.uniq(creators);
                         User.find({id: {$in: creators}}, function (err, creators) {
@@ -42,12 +81,14 @@ module.exports = {
                                     postComment.creator = creators[postComment.creator];
                                     return postComment;
                                 });
-                                res.view('post', {
+                                res.view('post/show', {
                                     post: post,
                                     moment: moment
                                 });
                             }
                         });
+                    } else {
+                        res.notFound();
                     }
                 });
     }
