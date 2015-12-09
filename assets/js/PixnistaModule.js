72d8f5dfd4b5933732cb0db73e5ca498004c9880
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-09 22:50:22 +0100

    Ajout de la directive dropzone pour l'utiliser dans la page de création d'un post

--- assets/js/PixnistaModule.js
+++ assets/js/PixnistaModule.js
@@ -1,1 +1,17 @@
-angular.module('PixnistaModule', ['toastr', 'compareTo']);
+angular.module('dropzone', []).directive('dropzone', function () {
+    return function (scope, element, attrs) {
+        var config, dropzone;
+
+        config = scope[attrs.dropzone];
+
+        // create a Dropzone for the element with the given options
+        dropzone = new Dropzone(element[0], config.options);
+
+        // bind the given event handlers
+        angular.forEach(config.eventHandlers, function (handler, event) {
+            dropzone.on(event, handler);
+        });
+    };
+});
+
+angular.module('PixnistaModule', ['toastr', 'compareField', 'dropzone']);
