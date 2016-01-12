9cc20f7d10dc9b2942b23300f26eb7009b925105
Pierre FABIER <pierre.fabier@gmail.com>
2016-01-12 14:34:34 +0100

    Ajout de documentation pour la méthode de findOne pour UserController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,6 +7,62 @@
  */
 
 module.exports = require('waterlock').actions.user({
+    // Il est interdit de lister les utilisateurs depuis l'API Rest
+    find: function (req, res) {
+        res.forbidden();
+    },
+    /**
+     * @api {get} /user/:id Get User by Id
+     * @apiName GetUserById
+     * @apiDescription Gets a User by its Id
+     * @apiGroup User
+     * @apiPermission none
+     *
+     * @apiParam {Number} id UserId
+     *
+     * @apiSuccess {Number} name User's display name
+     * @apiSuccess {Number} description User self description
+     * @apiSuccess {Number} email User's email
+     * @apiSuccess {Date} lastLoggedIn Date when the user last logged in
+     * @apiSuccess {String} gender Male ('M') or Female ('F')
+     * @apiSuccess {Date} birthdate Birthdate
+     * @apiSuccess {Number} height Height in cm
+     * @apiSuccess {Number} weight Weight in kilograms
+     * @apiSuccess {Number} image User's image Id (avatar)
+     * @apiSuccess {Number} language User's LanguageId
+     * @apiSuccess {Number} country User's CountryId
+     * @apiSuccess {Number} bodyType User's BodyTypeId
+     * @apiSuccess {Number} fashionStyles List of fashionStyles for this User
+     * @apiSuccess {Number} followedUsers Users that this User follows
+     * @apiSuccess {Number} followedByUsers Users that follows this User
+     * @apiSuccess {Number} blacklistedUsers Users that are blacklisted by this User
+     * @apiSuccess {Number} blacklistedByUsers Users that blacklisted this User
+     * @apiSuccess {Number} incomingMessages List of incoming Messages
+     * @apiSuccess {Number} outgoingMessages List of outgoing Messages
+     * @apiSuccess {Number} posts List of Posts created by this User
+     * @apiSuccess {Number} favoritePosts List of Posts favorited by this User
+     * @apiSuccess {Number} postVotes List of PostVotes created by this User
+     * @apiSuccess {Number} postComments List of PostComments created by this User
+     * @apiSuccess {Number} postCommentVotes List of PostCommentVotes created by this User
+     *
+     * @apiSuccess {Number} array.id ImageId
+     * @apiSuccess {String} array.name Name of Image
+     * @apiSuccess {String} array.filename Image's Filename
+     * @apiSuccess {String} array.description Image's Description
+     * @apiSuccess {String} array.imageData ImageDataId for this Image
+     * @apiSuccess {String} array.imageType ImageTypeId for this Image
+     * @apiSuccess {Date} array.createdAt Creation date
+     * @apiSuccess {Date} array.updatedAt Last update date
+     *
+     * @apiSampleRequest off
+     *
+     * @apiVersion 0.0.0
+     */
+    // findOne: function
+    /**
+     * Check the provided email address and password, and if they
+     * match a real user in the database, sign in.
+     */
     /**
      * @apiIgnore Not documented yet
      * @api {get} /XXXTODOXXX XXXTODOXXX
@@ -24,14 +80,6 @@ module.exports = require('waterlock').actions.user({
      * @apiSampleRequest off
      *
      * @apiVersion 0.0.0
-     */
-    // Il est interdit de lister les utilisateurs depuis l'API Rest
-    find: function (req, res) {
-        res.forbidden();
-    },
-    /**
-     * Check the provided email address and password, and if they
-     * match a real user in the database, sign in.
      */
     login: function (req, res) {
 
@@ -123,6 +171,24 @@ module.exports = require('waterlock').actions.user({
      * Log out.
      * (wipes `user` from the session)
      */
+    /**
+     * @apiIgnore Not documented yet
+     * @api {get} /XXXTODOXXX XXXTODOXXX
+     * @apiName XXXTODOXXX
+     * @apiDescription XXXTODOXXX
+     * @apiGroup XXXTODOXXX
+     * @apiPermission none
+     *
+     * @apiParam {Number} XXXTODOXXX
+     * @apiParam {Number} XXXTODOXXX
+     *
+     * @apiSuccess {String} XXXTODOXXX
+     * @apiSuccess {String} XXXTODOXXX
+     *
+     * @apiSampleRequest off
+     *
+     * @apiVersion 0.0.0
+     */
     logout: function (req, res) {
         // Wipe out the session (log out)
         req.session.user = undefined;
@@ -193,24 +259,7 @@ module.exports = require('waterlock').actions.user({
             }
         });
     },
-    /**
-     * @apiIgnore Not documented yet
-     * @api {get} /XXXTODOXXX XXXTODOXXX
-     * @apiName XXXTODOXXX
-     * @apiDescription XXXTODOXXX
-     * @apiGroup XXXTODOXXX
-     * @apiPermission Forbidden
-     *
-     * @apiParam {Number} XXXTODOXXX
-     * @apiParam {Number} XXXTODOXXX
-     *
-     * @apiSuccess {String} XXXTODOXXX
-     * @apiSuccess {String} XXXTODOXXX
-     *
-     * @apiSampleRequest off
-     *
-     * @apiVersion 0.0.0
-     */
+    // Interdit de supprimer un User depuis l'API
     destroy: function (req, res) {
         res.forbidden();
     }
