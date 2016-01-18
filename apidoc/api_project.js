07bd12fe5d217a80a100053f564458a0a0ace1e9
Pierre FABIER <pierre.fabier@gmail.com>
2016-01-18 12:52:06 +0100

    Dépot de la documentation générée apr apidoc
    Code pour générer à nouveau la doc, ou la mettre à jour :
    apidoc -v -i api -o apidoc
    
    Avec suppression de la doc existante :
    rm -rf apidoc && apidoc -v -i api -o apidoc

---
+++ apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "pixnista",
+  "version": "0.0.0",
+  "description": "Pixnista API documentation",
+  "title": "Pixnista API documentation",
+  "url": "https://pixnista.herokuapp.com/api",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-01-18T11:50:31.999Z",
+    "url": "http://apidocjs.com",
+    "version": "0.14.0"
+  }
+});
